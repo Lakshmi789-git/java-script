1c9851b210704bd5146713988a2ec393c80ac3c2
lakshmi789-git <lakshmilakshmiri807@gmail.com>
2024-09-18 13:54:40 +0530

    commit

---
+++ Two.js
@@ -0,0 +1,18 @@
+console.log("lakshmi.k.n")
+console.log(22082002)
+console.log("sai")
+console.log
+    username:"Lakshmi"
+    DOB:22/8/2002
+    Place:Bangalore
+    mobileNumber: 8861944349
+
+
+
+
+
+
+console.log("1"+1)
+console.log(1-1)
+console.log(1+1)
+
