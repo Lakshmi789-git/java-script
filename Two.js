cdaf2c2118ae9b17366dbcd7fd7868effb35cbd4
lakshmi789-git <lakshmilakshmiri807@gmail.com>
2024-09-19 12:49:30 +0530

    checking

--- Two.js
+++ Two.js
@@ -1,15 +1,13 @@
 console.log("lakshmi.k.n")
 console.log(22082002)
 console.log("sai")
-console.log
-    username:"Lakshmi"
-    DOB:22/8/2002
-    Place:Bangalore
-    mobileNumber: 8861944349
-
-
-
-
+console.log({
+    username:"Lakshmi",
+    DOB:22/8/2002,
+    Place:"Bangalore",
+    mobileNumber: 9861944340
+})
+    
 
 
 console.log("1"+1)
--- one.js
+++ one.js
@@ -5,3 +5,33 @@ console.log(false)
 console.log("prostack")
 
 console.log(typeof (true))
+
+// x=5;
+// console.log(X);
+
+message="good morning";
+console.log(message);
+
+array = [1,2,3,4,5,];
+console.log(array)
+
+emp= {
+    username:"prostack",
+    gender:"fe-male"
+    
+}
+console.log(emp);
+
+//let,const
+//var
+
+//let x=5;
+
+
+
+age=22
+console.log(age)
+
+
+
+
